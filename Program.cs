b4914b556dedff0128e03a968b5d4bee251a232b
Tamas Vicze <tomvicze96@hotmail.com>
2021-07-05 08:45:26 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Checkpoint_2_Tamas_Vicze
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
